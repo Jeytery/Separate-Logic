3288231372ce13e57ec8961a65ea0e29678d5033
Jeytery <ostapchenko.dmytro@a-bank.com.ua>
2023-09-08 16:47:41 +0300

    Initial Commit

---
+++ test-logic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test-logic
+//
+//  Created by Dmytro Ostapchenko on 08.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
